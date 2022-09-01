8261288fd2cc4adc8046e5c7080422cb5065f91c
Oksana <briliant621621@gmail.com>
2022-09-01 18:08:08 +0300

    Added StringInTheards.java

--- src/main/java/example2/StringInTheards.java
+++ src/main/java/example2/StringInTheards.java
@@ -2,9 +2,30 @@ package main.java.example2;
 
 public class StringInTheards {
     public static void main(String[] args) {
-        StringInTheard stringInTheard = new StringInTheard();
-        Thread thread1 = new Thread(stringInTheard);
-        thread1.start();
+        new Thread(() -> {
+            new MetodsForTheards().number();
+        }).start();
+        new Thread(() -> {
+            try {
+                new MetodsForTheards().buzz();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        new Thread(() -> {
+            try {
+                new MetodsForTheards().fizz();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
 
+        new Thread(() -> {
+            try {
+                new MetodsForTheards().fizzbuzz();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 }
