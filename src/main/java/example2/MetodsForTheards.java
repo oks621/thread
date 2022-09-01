c398c1a3432a6fc3c8c4db1efc55ffd60e6e2189
Oksana <briliant621621@gmail.com>
2022-09-01 21:25:49 +0300

    Added StringInTheards.java

--- src/main/java/example2/MetodsForTheards.java
+++ src/main/java/example2/MetodsForTheards.java
@@ -2,14 +2,15 @@ package main.java.example2;
 
 public class MetodsForTheards {
 
-    public void number() {
-        while (StringInTheard.j < 15) {
+
+   public void number()  {
+        while (StringInTheard.j<15) {
             synchronized (StringInTheard.class) {
 
-                if (StringInTheard.j % 5 != 0 && StringInTheard.j % 3 != 0) {
-                    System.out.print(" " + StringInTheard.j);
-                    ++StringInTheard.j;
-                }
+                if (StringInTheard.j % 5 != 0 && StringInTheard.j % 3 != 0)
+                {
+                    System.out.print( " " + StringInTheard.j );
+                    ++ StringInTheard.j;  }
 
                 StringInTheard.class.notifyAll();
             }
@@ -19,39 +20,34 @@ public class MetodsForTheards {
 
 
     public void fizz() throws InterruptedException {
-        while (StringInTheard.j < 15) {
-            synchronized (StringInTheard.class) {
+        while (StringInTheard.j<15){
+            synchronized (StringInTheard.class){
                 StringInTheard.class.wait();
 
                 if (StringInTheard.j % 3 == 0 && StringInTheard.j % 5 != 0) {
                     System.out.print(" fizz");
-                    ++StringInTheard.j;
-                }
+                    ++ StringInTheard.j; }
             }
         }
     }
 
     public void buzz() throws InterruptedException {
-        while (StringInTheard.j < 15) {
-            synchronized (StringInTheard.class) {
+        while (StringInTheard.j<15){
+            synchronized (StringInTheard.class){
                 StringInTheard.class.wait();
                 if (StringInTheard.j % 3 != 0 && StringInTheard.j % 5 == 0) {
                     System.out.print(" buzz");
-                    ++StringInTheard.j;
-                }
+                    ++ StringInTheard.j; }
             }
-        }
-    }
+        }}
 
     public void fizzbuzz() throws InterruptedException {
-        while (StringInTheard.j < 15) {
-            synchronized (StringInTheard.class) {
+        while (StringInTheard.j<15){
+            synchronized (StringInTheard.class){
                 StringInTheard.class.wait();
                 if (StringInTheard.j % 5 == 0 && StringInTheard.j % 3 == 0) {
-                    System.out.print(" fizzbuzz");
-                    ++StringInTheard.j;
-                }
+                    System.out.print(" fizzbuzz ");
+                    ++ StringInTheard.j; }
             }
-        }
-    }
+        }}
 }
--- src/main/java/example2/StringInTheards.java
+++ src/main/java/example2/StringInTheards.java
@@ -1,31 +1,33 @@
 package main.java.example2;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class StringInTheards {
     public static void main(String[] args) {
-        new Thread(() -> {
-            new MetodsForTheards().number();
-        }).start();
-        new Thread(() -> {
+        ExecutorService service = Executors.newFixedThreadPool(4);
+        service.execute(() -> new MetodsForTheards().number());
+        service.execute(() -> {
             try {
                 new MetodsForTheards().buzz();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }).start();
-        new Thread(() -> {
+        });
+        service.execute(() -> {
             try {
                 new MetodsForTheards().fizz();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }).start();
-
-        new Thread(() -> {
+        });
+        service.execute(() -> {
             try {
                 new MetodsForTheards().fizzbuzz();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }).start();
+        });
+
     }
 }
